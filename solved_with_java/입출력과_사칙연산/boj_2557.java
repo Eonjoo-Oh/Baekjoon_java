6a6d614176568e95c7a8db16eb485228023fe239
Eonjoo-Oh <eonjoo@Ejayui-MacBookAir.local>
2022-12-10 00:52:26 +0900

    2557

---
+++ solved_with_java/입출력과_사칙연산/boj_2557.java
@@ -0,0 +1,10 @@
+package 입출력과_사칙연산;
+
+/**
+ * @author Eonjoo Oh (eonjoo29@hanmail.net)
+ */
+public class boj_2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
