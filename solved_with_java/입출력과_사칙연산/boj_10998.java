6900d6e3ece5655ef6f0ff8fbe55431b7e274c82
Eonjoo-Oh <eonjoo@Ejayui-MacBookAir.local>
2022-12-11 02:21:56 +0900

    10998

---
+++ solved_with_java/입출력과_사칙연산/boj_10998.java
@@ -0,0 +1,19 @@
+package 입출력과_사칙연산;
+
+/**
+ * @author Eonjoo Oh (eonjoo29@hanmail.net)
+ */
+import java.util.Scanner;
+
+public class boj_10998 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        System.out.println(A * B);
+
+        in.close();
+    }
+}
